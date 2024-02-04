25c813e8012667c4afa4a37ecee7dc871948411b
Raphael Marques <jose.raphael.marques@gmail.com>
2024-02-04 20:04:24 -0300

    add unity tests to deploy workflows

--- src/modules/auth/application/use-cases/request-password-reset/request-password-reset.usecase.ts
+++ src/modules/auth/application/use-cases/request-password-reset/request-password-reset.usecase.ts
@@ -19,30 +19,18 @@ export class RequestPasswordResetUseCase {
   ) {}
 
   async execute(input: RequestPasswordResetDto): Promise<void> {
-    console.log('RequestPasswordResetUseCase.execute 1', input);
-
     validateDto(input, requestPasswordResetDtoSchema);
-
-    console.log('RequestPasswordResetUseCase.execute 2');
 
     const user = await this.userRepository.findByEmail(new Email(input.email));
     if (!user) return;
 
-    console.log('RequestPasswordResetUseCase.execute 3', user);
-
     const request = ResetPasswordRequest.create({ userId: user.id });
 
-    console.log('RequestPasswordResetUseCase.execute 4', request);
-
     await this.resetPasswordRequestRepository.add(request);
-
-    console.log('RequestPasswordResetUseCase.execute 5');
 
     await this.emailService.sendUpdatePasswordEmail({
       email: new Email(input.email),
       token: request.token.value,
     });
-
-    console.log('RequestPasswordResetUseCase.execute 6');
   }
 }
